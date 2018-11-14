b3611ca2803a106c56355acf123ec1fd8ccce449
AnthonyChitwood <44858806+AnthonyChitwood@users.noreply.github.com>
2018-11-14 05:50:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-     self.redirect("https://www.noproblemcash.com/", True)
+     self.redirect("https://www.mb01.com/lnk.asp?o=4849&c=918273&a=279700&l=3705", True)
 		# and this is the main code
 
 app = webapp2.WSGIApplication([
